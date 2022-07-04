2051d5d54e96696b5c7eb46df88ddd820eb22dd5
makifoeren <makif.oeren@gmail.com>
2022-07-04 15:15:14 +0200

    first commit

---
+++ src/test/java/Get01.java
@@ -0,0 +1,75 @@
+
+
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+
+public class Get01 {
+
+
+    @Test
+    public void test01() {
+
+        //Given
+        //            https://restful-booker.herokuapp.com/booking/3
+        //        When
+        //            User sends a GET Request to the url
+        // i) Set the URL
+
+        String url = "https://restful-booker.herokuapp.com/booking";
+
+        // ii) Set the expected data(POST-PUT-PATCH)
+
+        // iii) Type code to send request (POST-PUT-PATCH)
+
+        Response response = given().when().get(url);
+
+        // response.prettyPeek();
+        // response.prettyPrint();
+
+        // iii) Do Assertion
+
+
+        //        Then
+        //            HTTP Status Code should be 200
+        //        And
+        //            Content Type should be JSON
+        //        And
+        //            Status Line should be HTTP/1.1 200 OK
+
+        response.then().assertThat().
+                statusCode(200).
+                contentType("application/json").
+                statusLine("HTTP/1.1 200 OK");
+
+        // status code nasil yazdirilir
+
+        System.out.println("Status Code = " + response.statusCode());
+
+        // Content Type nasil yazdirilir
+
+        System.out.println("Content Type : " + response.contentType());
+
+        // Status Line nasil yazdirilir
+
+        System.out.println("Status Line : " + response.statusLine());
+
+
+        // Header nasil yazdirilir
+
+        //System.out.println(response.header("Connetion"));
+        //System.out.println(response.header("User-Agent"));
+
+        // Headers nasil yazdirilir
+
+        System.out.println("Headers :\n" + response.headers());
+
+        // Time nasil yazdirilir
+
+        System.out.println("response.getTime() = " + response.getTime());
+
+    }
+}
---
+++ src/test/java/Tanim.java
@@ -0,0 +1,24 @@
+public class Tanim {
+
+
+ /*
+    1) Postman manuel API testi icin kollanilir
+    2) Api Otomasyon rest-assured Library kullaniyoruz
+    3) Otomasyon kodlarinin yazimi icin su adimlari iziliyoruz
+        a) Gereksinimleri anlama
+        b) Test case yazma
+            i) Test case yazimi icin 'Gherkin Language' kullaniyoruz
+                'Gherkin' bazi keywordlere sahpbunlar
+                x) Given: On kosullar
+                y) When: Yapacagimiz aksiyonlar icin --> Get,Put
+                z) Then: Ciktilar veya donutler icin --> Assert
+                t) And: Coklu islemler icin
+
+         c) Testin kodunun yazimi
+            i) Set the URL
+            ii) Set the expected data(POST-PUT-PATCH)
+            iii) Type code to send request (POST-PUT-PATCH)
+            iiii) Do Assertion
+
+         */
+}
