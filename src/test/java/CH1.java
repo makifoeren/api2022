f6125921dd5f64c9d550af747b4b7422655415de
makifoeren <makif.oeren@gmail.com>
2022-07-05 09:32:47 +0200

    05.07

---
+++ src/test/java/CH1.java
@@ -0,0 +1,29 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CH1 {
+
+
+    @Test
+    public void test01() {
+
+
+
+
+        String urll="https://restful-booker.herokuapp.com/booking/20297";
+
+        Response res=given().when().get(urll);
+
+        res.prettyPrint();
+
+
+        System.out.println("res.statusCode() = " + res.statusCode());
+
+        res.then().assertThat().statusCode(200).contentType("application/json; charset=utf-8").
+                statusLine("HTTP/1.1 200 OK").header("Server","Cowboy");
+
+
+    }
+}
